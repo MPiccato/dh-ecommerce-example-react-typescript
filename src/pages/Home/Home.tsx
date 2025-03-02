7ff1bf84b05c12f41cc2672aafd87497da687d16
MP Data - Trading <mpiccato@me.com>
2025-03-02 09:38:57 -0300

    hecha la llamada a la API (falsa)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,32 @@
-import { Hero } from "../../components/ui/Hero/Hero"
+import { useEffect, useState } from "react";
+import { Hero } from "../../components/ui/Hero/Hero";
 
 
 const Home = () => {
+
+
+  const [productos, setProducts] = useState([])
+
+  const getProducts = async () => {
+    try{
+        const response = await fetch(`http://localhost:3000/products`)
+        const data = await response.json()
+        setProducts(data)
+    }
+    
+    catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  useEffect(() => {
+    getProducts()
+  },[])
+
+  console.log(productos)
+
+
   return (
     <>
     <Hero/>
